f2b256ae93e1266fe7e88128e5bfb41711934d8b
Luiz Omena <luizfilipeyep@gmail.com>
2022-04-26 08:36:34 -0300

    finish hero

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,38 @@
         <a href="#">Filmes</a>
         <a href="#">Minha lista</a>
     </header>
-    <section class="hero"></section>
+
+    <section class="hero">
+        <img src="./assets/stranger-things-poster.png" alt="stranger-things-poster">
+
+        <div class="hero-info">
+            <img src="./assets/stranger-things-logo.svg" alt="stranger-things-logo">
+
+            <div class="hero-tags">
+                <ul>
+                    <li>Sinistro</li>
+                    <li>Empolgante</li>
+                    <li>Adolescente</li>
+                </ul>
+            </div>
+
+            <div class="hero-options">
+                <a class="my-list-button">
+                    <img src="./assets/icons/plus-faw-icon.svg" alt="plus-faw-icon">
+                    Minha lista
+                </a>
+                <a class="play-button">
+                    <img src="./assets/icons/play-faw-icon.svg" alt="play-faw-icon">
+                    Assistir
+                </a>
+                <a class="details-button">
+                    <img src="./assets/icons/info-fi-icon.svg" alt="info-fi-icon">
+                    Detalhes
+                </a>
+            </div>
+        </div>
+    </section>
+
     <nav class="float"></nav>
 </div>
 </template>
@@ -59,8 +90,125 @@ header {
 
 section.hero {
     width: 100%;
-    height: 401px;
-    background: blue;
+    height: auto;
+    position: relative;
+
+    img {
+        width: 100%;
+        height: auto;
+    }
+
+    .hero-info {
+        width: 100%;
+        height: 176px;
+        position: absolute;
+        z-index: 2;
+        bottom: 0;
+        background: linear-gradient(0deg, #141414 10%, transparent);
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+
+        img {
+            width: 188px;
+            height: auto;
+            position: relative;
+        }
+
+        .hero-tags {
+            color: white;
+            font-size: 12px;
+
+            ul {
+                list-style: none;
+
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                gap: 18px;
+                margin-left: -40px;
+
+                li::before {
+                    content: "\2022";  /* Add content: \2022 is the CSS Code/unicode for a bullet */
+                    color: $red; /* Change the color */
+                    font-weight: bold; /* If you want it to be bold */
+                    display: inline-block; /* Needed to add space between the bullet and the text */
+                    width: 1em; /* Also needed for space (tweak if needed) */
+                    margin-left: -1em; /* Also needed for space (tweak if needed) */
+                }
+                li:nth-child(1)::before {
+                    content: ""
+                }
+            }
+        }
+
+        .hero-options {
+            width: 100%;
+            height: 43px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 25px;
+
+            .my-list-button {
+                img {
+                    width: 15px;
+                }
+
+                width: 35px;
+                height: 35px;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                color: white;
+                font-size: 10px;
+                font-weight: bold;
+                white-space: nowrap;
+                border-radius: 50px;
+            }
+
+            .play-button {
+                img {
+                    width: 11px;
+                }
+
+                font-size: 10px;
+                font-weight: bold;
+                outline: none;
+                color: $black;
+                text-decoration: none;
+                background: $white;
+                border-radius: 2px;
+                width: 100px;
+                height: 25px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                gap: 10px;
+            }
+
+            .details-button {
+                img {
+                    width: 15px;
+                }
+
+                width: 35px;
+                height: 35px;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                color: white;
+                font-size: 10px;
+                font-weight: bold;
+                white-space: nowrap;
+                border-radius: 50px;
+            }
+        }
+    }
 }
 
 nav.float {
